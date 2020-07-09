704be0ff4b7a53670f0aecc10c283618c5f43c30
Schuttert <j.schuttert@student.tue.nl>
2020-07-09 17:54:33 +0200

    - changed movement to move background instead of character - added slightly better background, high res

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import game_objects as gob
 pg.init()  # start pygame
 screen = pg.display.set_mode(settings.screen_size)
 player = gob.newentity('player', (800-120, 350), 1, graphics = pg.image.load(r"./PS.png"), graphics_size = (240,240)) # creates a new entity in the foreground
-background = gob.newentity('background', (0,-350), 0, graphics = pg.image.load(r"./bg.png"), graphics_size = (1600,1600))  # creates a background entity
+background = gob.newentity('background', (0,-350), 0, graphics = pg.image.load(r"./bg2.png"), graphics_size = (1600*2,1600*2))  # creates a background entity
 
 
 # operation defenitions (like drawing frames, calculating movement, ect)
@@ -15,14 +15,15 @@ def move(entity, x=0, y=0):  # change location of an entity
     entity.loc[0] = entity.loc[0] + x*settings.player_speed  # change x
     entity.loc[1] = entity.loc[1] + y*settings.player_speed  # change y
     entity.centerupdate()  # update entities center
-    if (entity.center[0] <= settings.screen_size[0]/6) or (entity.center[0] >= (5*settings.screen_size[0]/6)):
-        entity.loc[0] = entity.loc[0] - x*settings.player_speed  # change x back
-        print("moved far... should move background instead")
-    if (entity.center[1] <= settings.screen_size[1]/3) or (entity.center[1] >= (2*settings.screen_size[1]/3)):
-        entity.loc[1] = entity.loc[1] - y*settings.player_speed  # change y back
-        print("moved far... should move background instead")
-    entity.centerupdate()  # update entities center
 
+def move_screenbound(entity, x=0, y=0):  # move the background sprite in opposite direction to fake movement
+    entity.loc[0] = entity.loc[0] - x*settings.player_speed  # change x
+    entity.loc[1] = entity.loc[1] - y*settings.player_speed  # change y
+    entity.centerupdate()  # update entities center
+    if (entity.loc[0] > 0) or (entity.loc[0] < -1*(entity.size[0]-settings.screen_size[0])):
+        entity.loc[0] = entity.loc[0] + x*settings.player_speed  # change x back
+    if (entity.loc[1] > 0) or (entity.loc[1] < -1*(entity.size[1]-settings.screen_size[1])):
+        entity.loc[1] = entity.loc[1] + y*settings.player_speed  # change y back
 
 def draw(): # frame drawing
     screen.fill((255,0,255))  # removes all drawings
@@ -60,7 +61,7 @@ while run:  # actual loop
         else: move_x = 0
 
     # entity calculating (movement, collisions, ect)
-    move(player, move_x, move_y)
+    move_screenbound(background, move_x, move_y)
 
     # screen update (clears prev draws, and draws the new data)
     draw()
