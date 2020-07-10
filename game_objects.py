f95cb0de1ce006a4b63812c39c3664b457efa6ec
Schuttert <j.schuttert@student.tue.nl>
2020-07-10 17:57:56 +0200

    Added walkcycle!!!
    It's fucked up...

--- game_objects.py
+++ game_objects.py
@@ -1,9 +1,18 @@
 '''
 Game structure:
+- The game state is saved in a 'world' class
+    - world contains things like:
+        - triggers
+        - events
+        - lighting (???)
+        - globally needed atributes (eg. sprite states)
+        - interactables information
+    - world gets referenced if 2 entities need to interact with each other (outside collisions),
+      or need information on each other, not obtainable otherwise.
 - All things shown on screen are an 'entity'
     - Entities have attributes allowing them to be drawn
     - Entities may exist offscreen
-    - Entities may have an lifetime, after which they may not be drawn again and
+    - Entities may have a lifetime, after which they may not be drawn again and
       will be removed
 - All collidable entities are called 'collision objects'
     - collision objects belong to the 'collisob' class
@@ -20,21 +29,39 @@ checked for collisions every tick
 entity_world = []     # collection of all entities, sorted by age (descending)
 collision_world = []  # collection of all collision objects, unsorted
 
+# class that stores information needed for entity interactions and dependency
+# NOTE: attributes are assigned as needed, and removed as needed, please manage this manually!
+# NOTE2: only ONE instance of this class should exists: 'game_world'!!!!
+class world:
+    pass
+game_world = world()
 # class that contains neccecary information to draw something on the screen
 class entity:
-    def __init__(self, name, location, layer, graphics=None, graphics_size=None, lifetime=None):
+    def __init__(self, name, location, layer, graphics=[None,None,None,None], graphics_size=None, lifetime=None):
         if graphics and not graphics_size: raise ValueError ("graphics specified but no size was given!")
         self.name = name  # name of the entity
         self.loc = [location[0], location[1]]  # coördinates of the entity [x, y]
+        self.direction = "UP"  # facing direction of the entity, defealt to up
         self.center = [location[0]+graphics_size[0]/2, location[1]+graphics_size[1]/2]  # center of the entity
-        self.size = (graphics_size[0], graphics_size[1])  # size of the graphics images
-        self.graphics = graphics # optional graphics for the entity
+        self.size = (graphics_size[0], graphics_size[1])  # size of the graphics images in forward position
+        self.graphics = {  # optional graphics for the entity
+            "LEFT"  : graphics[0],  # list containing images for left move
+            "RIGHT" : graphics[1],  # list containing images for right move
+            "UP"    : graphics[2],  # list containing images for up move
+            "DOWN"  : graphics[3]  # list containing images for down move
+        }
         self.age = None # integer nr of frames since the object appeared on the screen
+        self.cycle_cur = 0  # frame in an optional cycle that should be drawn
+        self.cycle_len = 9  # length of the optional cycle that should be drawn (defealt 9)
         self.lifetime = lifetime # nr of maximum frames the object is allowed to appear
         self.layer = layer  # draw layer the sprite should be drawn to.
 
-    def centerupdate(self):
+    def centerupdate(self):  # updates the center of image attribute of the entity
         self.center = [self.loc[0] + self.size[0]/2, self.loc[1] + self.size[1]/2]  # updates center of the entity
+
+    def cycle_update(self):  # updates any cycles present in the entity attributes
+        if self.cycle_cur+1 >= self.cycle_len: self.cycle_cur = 0
+        else: self.cycle_cur = self.cycle_cur+1
 
 # entity class that has collision bounds
 class collisob(entity):
@@ -47,7 +74,7 @@ class collisob(entity):
             (self.loc[0], self.loc[1]+size[1])  # left lower corner
         ]
 
-def newentity(name, location, layer, graphics=None, graphics_size=None, lifetime=None):
+def newentity(name, location, layer, graphics=[None,None,None,None], graphics_size=None, lifetime=None):
     global entity_world
     newitem = entity(name, location, layer, graphics, graphics_size, lifetime)  # create new entity
     entity_world.append(newitem)  # append entity to world
--- main.py
+++ main.py
@@ -1,15 +1,23 @@
 import pygame as pg
 import settings
 import game_objects as gob
-
-# window + program setup
-pg.init()  # start pygame
-screen = pg.display.set_mode(settings.screen_size)
-player = gob.newentity('player', (800-72, 350+72), 1, graphics = pg.image.load(r"./PS3.png"), graphics_size = (144,144)) # creates a new entity in the foreground
-background = gob.newentity('background', (0,-350), 0, graphics = pg.image.load(r"./bg2.png"), graphics_size = (1600*2,1600*2))  # creates a background entity
-
+from os import getcwd
 
 # operation defenitions (like drawing frames, calculating movement, ect)
+
+def loadsprite(EntityPreFx, direction, cycletype=None, cyclelen=None):
+    # loadsprite loads images to use, and groups them for animation-cycles
+    # return either one image in list, or a tuple with the direction and image sequence
+    if cyclelen and cycletype:
+        returnlist = []  # list with loaded images to return
+        for n in range(cyclelen):
+            print(f"Loading {EntityPreFx}_{cycletype}_{direction}_{n}...",end='')
+            try:
+                returnlist.append(pg.image.load(f".\{EntityPreFx}_{cycletype}_{direction}_{n}.png"))
+                print("Complete!")
+            except pg.error: print("FAILED"); pg.quit(); raise  # quit on error
+        return (direction, returnlist)
+    else: return ("UP", [pg.image.load(f".\{EntityPreFx}_{direction}.png")])
 
 def move(entity, x=0, y=0):  # change location of an entity
     entity.loc[0] = entity.loc[0] + x*settings.player_speed  # change x
@@ -19,22 +27,42 @@ def move(entity, x=0, y=0):  # change location of an entity
 def move_screenbound(entity, x=0, y=0):  # move the background sprite in opposite direction to fake movement
     entity.loc[0] = entity.loc[0] - x*settings.player_speed  # change x
     entity.loc[1] = entity.loc[1] - y*settings.player_speed  # change y
+    moved = True  # sets a variable to remember wether the move was succesfull
     entity.centerupdate()  # update entities center
+
     if (entity.loc[0] > 0) or (entity.loc[0] < -1*(entity.size[0]-settings.screen_size[0])):
         entity.loc[0] = entity.loc[0] + x*settings.player_speed  # change x back
+        moved = False
     if (entity.loc[1] > 0) or (entity.loc[1] < -1*(entity.size[1]-settings.screen_size[1])):
         entity.loc[1] = entity.loc[1] + y*settings.player_speed  # change y back
+        moved = False
+
+    if moved: player.cycle_update()  # update which sprite to render in animation cycle
+    else: player.cycle_cur = 0  # reset animation cycle if player didn't move
 
 def draw(): # frame drawing
     screen.fill((255,0,255))  # removes all drawings
-    screen.blit(background.graphics,(background.loc[0], background.loc[1]))  # draws background sprites
+    screen.blit(background.graphics["UP"][0],(background.loc[0], background.loc[1]))  # draws background sprites
     for layer in range(settings.drawlayers, 0, -1): # draw layers
         for ent in gob.entity_world:
             if ent.layer == layer:
                 #pg.draw.rect(screen, (255,0,0), pg.Rect(ent.loc[0], ent.loc[1], 60, 90))
-                screen.blit(ent.graphics, (ent.loc[0],ent.loc[1]))
+                screen.blit(ent.graphics[ent.direction][ent.cycle_cur], (ent.loc[0],ent.loc[1]))
     pg.display.update() # send update to screen
 
+
+# window + program setup
+pg.init()  # start pygame
+screen = pg.display.set_mode(settings.screen_size)
+player = gob.newentity('player', (800-72, 350+72), 1, graphics_size = (144,360)) # creates a new entity in the foreground
+for indx, direction in enumerate(player.graphics.keys()):
+    print(f"assigning images to direction: graphics[{indx}]: {direction}...",end='')
+    player.graphics[direction] = loadsprite('P',direction,"WC",player.cycle_len)[1]
+    print("Complete!")
+
+print(player.graphics)
+background = gob.newentity('background', (0,-350), 0, graphics_size = (1600*2,1600*2))  # creates a background entity
+background.graphics["UP"] = loadsprite('BG2', "UP")[1]  # loads the singe sprite list into the 'up' graphics slot
 
 # main loop
 clock = pg.time.Clock()  # game clock, controls framerate
@@ -51,14 +79,19 @@ while run:  # actual loop
         pressed_keys = pg.key.get_pressed() # list of all keys that are pressed
         if pressed_keys[pg.K_UP]:
             move_y = -1
+            player.direction = "UP"
         elif pressed_keys[pg.K_DOWN]:
             move_y = 1
+            player.direction = "UP"
         else: move_y = 0
         if pressed_keys[pg.K_LEFT]:
             move_x = -1
+            player.direction = "LEFT"
         elif pressed_keys[pg.K_RIGHT]:
             move_x = 1
+            player.direction = "RIGHT"
         else: move_x = 0
+        if move_x == 0 and move_y == 0: player.direction = "UP"
 
     # entity calculating (movement, collisions, ect)
     move_screenbound(background, move_x, move_y)
